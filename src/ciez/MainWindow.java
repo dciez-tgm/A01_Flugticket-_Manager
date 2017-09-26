561240d19a43d7a7fefb40e38957febb0232ce07
David Ciez <dciez@student.tgm.ac.at>
2017-09-26 12:20:07 +0200

    Added and edited GUI elements

--- src/ciez/MainWindow.java
+++ src/ciez/MainWindow.java
@@ -52,6 +52,19 @@ public class MainWindow {
 		shlFlightPlanner.setSize(750, 500);
 		shlFlightPlanner.setText("Flight Planner\r\n");
 		
+		tOutput = new Text(shlFlightPlanner, SWT.BORDER | SWT.MULTI); //SWT.MULTI for multiple line output
+		tOutput.setText("Flight Information:" + "\n");
+		tOutput.setFont(SWTResourceManager.getFont("Courier", 10, SWT.NORMAL));
+		tOutput.setEditable(false);
+		tOutput.setBounds(475, 161, 224, 268);
+		//tOutput.append(""); to add text
+		tOutput.append("---   Origin    ---" + "\n");
+		tOutput.append("Country: " + "\n");
+		tOutput.append("Airport: " + "\n");
+		tOutput.append("--- Destination ---" + "\n");
+		tOutput.append("Country: " + "\n");
+		tOutput.append("Airport: " + "\n");
+		
 		Label lblTickts = new Label(shlFlightPlanner, SWT.NONE);
 		lblTickts.setFont(SWTResourceManager.getFont("System", 30, SWT.NORMAL));
 		lblTickts.setBounds(25, 25, 300, 50);
@@ -67,17 +80,47 @@ public class MainWindow {
 		lblDestination.setFont(SWTResourceManager.getFont("System", 17, SWT.NORMAL));
 		lblDestination.setBounds(238, 120, 160, 35);
 		
-		ComboViewer comboViewer = new ComboViewer(shlFlightPlanner, SWT.NONE);
-		Combo combo = comboViewer.getCombo();
-		combo.setBounds(25, 161, 200, 23);
+		Label lblCountryOrigin = new Label(shlFlightPlanner, SWT.NONE);
+		lblCountryOrigin.setFont(SWTResourceManager.getFont("Courier", 12, SWT.NORMAL));
+		lblCountryOrigin.setBounds(25, 161, 110, 18);
+		lblCountryOrigin.setText("Country");
 		
-		ComboViewer comboViewer_1 = new ComboViewer(shlFlightPlanner, SWT.NONE);
-		Combo combo_1 = comboViewer_1.getCombo();
-		combo_1.setBounds(238, 161, 200, 23);
+		Label lblCountryDestination = new Label(shlFlightPlanner, SWT.NONE);
+		lblCountryDestination.setText("Country");
+		lblCountryDestination.setFont(SWTResourceManager.getFont("Courier", 12, SWT.NORMAL));
+		lblCountryDestination.setBounds(238, 161, 110, 18);
 		
-		tOutput = new Text(shlFlightPlanner, SWT.BORDER);
-		tOutput.setEditable(false);
-		tOutput.setBounds(475, 161, 224, 268);
+		ComboViewer comboViewer = new ComboViewer(shlFlightPlanner, SWT.DROP_DOWN | SWT.BORDER);
+		Combo comboOriginCountry = comboViewer.getCombo();
+		comboOriginCountry.setBounds(25, 185, 200, 23);
+		//comboOrigin.add(""); too add items to the dropdownlist
+		/*
+		for(int i=1; i<121; i++){
+			comboOrigin.add("Item " + i);
+		}
+		*/
+		
+		ComboViewer comboViewer_1 = new ComboViewer(shlFlightPlanner, SWT.DROP_DOWN | SWT.BORDER);
+		Combo comboDestinationCountry = comboViewer_1.getCombo();
+		comboDestinationCountry.setBounds(238, 185, 200, 23);
+		
+		Label lblAirportOrigin = new Label(shlFlightPlanner, SWT.NONE);
+		lblAirportOrigin.setText("Airport");
+		lblAirportOrigin.setFont(SWTResourceManager.getFont("Courier", 12, SWT.NORMAL));
+		lblAirportOrigin.setBounds(25, 232, 110, 18);
+		
+		Label lblAirportDestination = new Label(shlFlightPlanner, SWT.NONE);
+		lblAirportDestination.setText("Airport");
+		lblAirportDestination.setFont(SWTResourceManager.getFont("Courier", 12, SWT.NORMAL));
+		lblAirportDestination.setBounds(238, 232, 110, 18);
+		
+		ComboViewer comboViewer_2 = new ComboViewer(shlFlightPlanner, SWT.NONE);
+		Combo comboOriginAirport = comboViewer_2.getCombo();
+		comboOriginAirport.setBounds(25, 256, 200, 23);
+		
+		ComboViewer comboViewer_3 = new ComboViewer(shlFlightPlanner, SWT.NONE);
+		Combo comboDestinationAirport = comboViewer_3.getCombo();
+		comboDestinationAirport.setBounds(238, 256, 200, 23);
 
 	}
 }
